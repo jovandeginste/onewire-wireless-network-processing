a289b9256277689e5349820b13e24f5cb4d42136
Jo Vandeginste <jo.vandeginste@gmail.com>
2015-09-12 17:34:19 +0200

    Read a string from the tty

--- receive.go
+++ receive.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"github.com/marpaia/graphite-golang"
 	"github.com/tarm/serial"
 	"gopkg.in/yaml.v2"
@@ -30,40 +31,6 @@ type Config struct {
 	Name_mapping map[string]string
 }
 
-func main() {
-	config, err := read_config(os.Args[1])
-
-	port_str := config.Receiver.Port_str
-	baud_rate := config.Receiver.Baud_rate
-
-	reset_tty(port_str, baud_rate)
-
-	sif, err := serial.OpenPort(&serial.Config{Name: port_str, Baud: baud_rate})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// try to connect a graphite server
-	Graphite, err := graphite.NewGraphite(config.Collector.Configuration.Host, config.Collector.Configuration.Port)
-
-	// if you couldn't connect to graphite, use a nop
-	if err != nil {
-		Graphite = graphite.NewGraphiteNop(config.Collector.Configuration.Host, config.Collector.Configuration.Port)
-	}
-
-	log.Printf("Value: %#v\n", config.Name_mapping["00000d0000000001"])
-
-	log.Printf("Loaded Graphite connection: %#v", Graphite)
-	Graphite.SimpleSend("stats.graphite_loaded", "1")
-
-	buf := make([]byte, 128)
-	n, err := sif.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("%q", buf[:n])
-}
-
 func read_config(filename string) (config Config, err error) {
 	data, _ := ioutil.ReadFile(filename)
 	if err != nil {
@@ -88,4 +55,34 @@ func reset_tty(port_str string, baud_rate int) (err error) {
 		return
 	}
 	return
+}
+
+func main() {
+	config, err := read_config(os.Args[1])
+
+	port_str := config.Receiver.Port_str
+	baud_rate := config.Receiver.Baud_rate
+
+	reset_tty(port_str, baud_rate)
+
+	sif, err := serial.OpenPort(&serial.Config{Name: port_str, Baud: baud_rate})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// try to connect a graphite server
+	Graphite, err := graphite.NewGraphite(config.Collector.Configuration.Host, config.Collector.Configuration.Port)
+	Graphite.Prefix = config.Collector.Configuration.Prefix
+
+	log.Printf("Value: %#v\n", config.Name_mapping["00000d0000000001"])
+
+	log.Printf("Loaded Graphite connection: %#v", Graphite)
+	Graphite.SimpleSend("stats.graphite_loaded", "1")
+
+	reader := bufio.NewReader(sif)
+	reply, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	log.Println(reply)
 }
